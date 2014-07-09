ae0b4faae26a3626f85ecfd530a5485833f96511
flopcoder <flopcoder.88@gmail.com>
2014-07-10 01:31:05 +0600

    INIT

---
+++ src/patterns/abstract_factory/AbstractFactoryTest.java
@@ -0,0 +1,13 @@
+package patterns.abstract_factory;
+
+import factory.DesignTest;
+
+/**
+ * Created by Flop Coder on 7/10/14.
+ */
+public class AbstractFactoryTest implements DesignTest {
+    @Override
+    public void test() {
+        System.out.println("This is AbstructFactoryTest");
+    }
+}
